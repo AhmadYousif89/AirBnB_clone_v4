6e3e47dfb04e4a2e7439dc4c4fdc09d144a9bf96
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-06-02 03:32:02 +0300

    Refactor code to load places dynamically using JS

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,13 +1,11 @@
 const $ = window.$;
 
 $(function () {
-  $('.filters .amenities').append('<small class="selected"></small>');
-  // Call the function when the page loads
-  loadCheckedAmenities();
-  // Call the function when the checkbox changes
-  $('input[type="checkbox"]').change(loadCheckedAmenities);
-  // Check the API status when the page loads
   checkAPIStatus();
+  $('.filters .amenities').append('<small class="selected"></small>');
+  loadCheckedAmenities();
+  $('input[type="checkbox"]').change(loadCheckedAmenities);
+  loadPlaces();
 });
 
 function loadCheckedAmenities() {
@@ -41,5 +39,53 @@ function checkAPIStatus() {
   res.fail(() => {
     $('#api_status').removeClass('available');
     $('#api_status').attr('title', ' API Status : offline ');
+  });
+}
+
+function loadPlaces() {
+  const url = 'http://127.0.0.1:5001/api/v1/places_search';
+  const res = $.ajax({
+    url,
+    type: 'POST',
+    data: JSON.stringify({}),
+    contentType: 'application/json'
+  });
+  res.done(function (places) {
+    console.log(places);
+    for (const place of places) {
+      $('.places .wrapper').append(
+        `<article data-id="${place.id}">
+          <h2 title="${place.name}">${place.name}</h2>
+          <div class="price_by_night">
+              <span>$${place.price_by_night}</span>
+          </div>
+          <div class="information">
+            <div class="max_guest">
+              <img src="../static/images/icon_group.png" alt="icon">
+              <span>
+                ${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}
+              </span>
+            </div>
+            <div class="number_rooms">
+              <img src="../static/images/icon_bed.png" alt="icon">
+              <span>
+                ${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}
+              </span>
+            </div>
+            <div class="number_bathrooms">
+              <img src="../static/images/icon_bath.png" alt="icon">
+              <span>
+                ${place.number_bathrooms} Bedroom${
+          place.number_bathrooms !== 1 ? 's' : ''
+        }</span>
+            </div>
+          </div>
+          <div class="description"><p>${place.description}</p></div>
+        </article>`
+      );
+    }
+  });
+  res.fail(() => {
+    console.log('Failed to load places');
   });
 }
