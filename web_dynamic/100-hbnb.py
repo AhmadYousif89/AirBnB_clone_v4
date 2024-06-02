c325cd0b158b1f55fe18bc5abba21f7842553dfb
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-06-02 21:40:15 +0300

    Add web_dynamic/100-hbnb.py Flask route

---
+++ web_dynamic/100-hbnb.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application with a route '/2-hbnb/'
+"""
+from uuid import uuid4
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Closes the current SQLAlchemy Session"""
+    storage.close()
+
+
+idx = 100
+
+
+@app.route('/{}-hbnb'.format(idx), strict_slashes=False)
+def hbnb_filters():
+    """Displays a list of all States, Cities and Amenities"""
+    states = list(storage.all("State").values())
+    amenities = list(storage.all("Amenity").values())
+    return render_template(
+        '{}-hbnb.html'.format(idx),
+        cache_id=uuid4(),
+        states=states,
+        amenities=amenities,
+        js_file_prefix='{}-'.format(idx),
+    )
+
+
+if __name__ == '__main__':
+    app.run()
